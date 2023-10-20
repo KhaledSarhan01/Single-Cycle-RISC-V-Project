3a9a4037bffbe0ab4c82e4fa3fbe8efa0a33c031
Khaled Sarhan <khaledsarhaan@gmail.com>
2023-10-20 19:52:59 +0300

    Testing Finished on ALU and Sign Extender

--- Modules/Sign Extender/TB.v
+++ Modules/Sign Extender/TB.v
@@ -1,32 +1,47 @@
 module SignExtender_tb;
 
   // Define testbench inputs and outputs
-  reg [15:0] inputData;
-  wire [31:0] extendedData;
+  reg  [1:0] ImmSrc;
+  reg  [31:0] data_in;
+  wire [31:0] data_out;
 
   // Instantiate the SignExtender module
-  SignExtender uut (
-    .inputData(inputData),
-    .extendedData(extendedData)
+  sign_extender uut (
+    .ImmSrc(ImmSrc),
+    .data_in(data_in),
+    .data_out(data_out)
   );
 
   // Test sequence
   initial begin
     // Test with positive number (no sign bit change)
-    inputData = 16'h1234;
-    #10;
-    
+    data_in = 32'b0110_1111_1110_1001_1010_0110_0101_1110;
+    ImmSrc =2'b00;
+    #100;
+    ImmSrc =2'b01;
+    #100;
+    ImmSrc =2'b10;
+    #100;
+    ImmSrc =2'b11;
+    #100;
     // Test with negative number (sign bit extension)
-    inputData = 16'hFEDC;
-    #10;
+    data_in = 32'b1001_0101_1100_0110_1110_0110_0101_1110;
+    ImmSrc =2'b00;
+    #100;
+    ImmSrc =2'b01;
+    #100;
+    ImmSrc =2'b10;
+    #100;
+    ImmSrc =2'b11;
+    #100;
     
     // End simulation
     $finish;
   end
 
   // Display input and output data
-  always begin
-    $display("Input: %h, Extended: %h", inputData, extendedData);
+  initial begin
+    $monitor("Input: %b, Extended: %b", data_in, data_out);
   end
 
 endmodule
